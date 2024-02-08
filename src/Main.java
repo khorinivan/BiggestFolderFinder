a26d1226bfd782a5886669867ea23282a94a946e
ivan_khorin <prisrak@mail.ru>
2024-02-08 21:01:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String folderPath = "C:\\Users\\Иван\\Desktop";
+        File file = new File(folderPath);
+
+
+        System.out.println();
+
+        System.out.println(file.length());
+
+
+
+    }
+}
